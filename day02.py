eb487a3b225c3d3f19ce245059de8ca1d67f6725
Greg Bullmer <gsbullmer@gmail.com>
2017-12-04 13:05:57 -0600

    feat: Finish day02 puzzle 2

--- day02.py
+++ day02.py
@@ -2,8 +2,20 @@ from utils import get_input
 
 
 def corruption_checksum(data):
-    sum_digits = list(map(lambda x: max(x) - min(x), data))  # part 1
+    # sum_digits = list(map(lambda row: max(row) - min(row), data))  # part 1
+    sum_digits = list(map(lambda row: divisor(row), data))  # part 2
     return sum(sum_digits)
+
+
+def divisor(row):
+    row.sort()
+    row.reverse()
+    for idx1 in range(len(row)):
+        for idx2 in range(idx1 + 1, len(row)):
+            if row[idx1] % row[idx2] is 0:
+                return int(row[idx1] / row[idx2])
+    return
+
 
 if __name__ == "__main__":
     puzzle_input = get_input('day02')
