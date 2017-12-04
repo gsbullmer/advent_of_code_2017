b4935d461046e05733873ab2d562f434d9ebf01f
Greg Bullmer <gsbullmer@gmail.com>
2017-12-04 10:58:10 -0600

    feat: Finish day 1 puzzle 2

--- day01.py
+++ day01.py
@@ -3,7 +3,9 @@ from utils import get_input
 
 def reverse_captcha(string):
     digits = list(map(lambda d: int(d), string))
-    next_digits = iter(digits[1:] + digits[:-1])
+    # offset = 1  # part 1: next digit in circular list
+    offset = int(len(digits) / 2)  # part 2: digit halfway around circular list
+    next_digits = iter(digits[offset:] + digits[:-offset])
     sum_digits = list(filter(lambda x: x == next(next_digits), digits))
     return sum(sum_digits)
 
